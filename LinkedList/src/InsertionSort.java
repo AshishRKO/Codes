afc071a71fa61905cb721493847adba68235e5e9
Ashish Barthwal <ashishb619@gmail.com>
2016-05-01 11:03:21 +0530

    Partition List

--- LinkedList/src/InsertionSort.java
+++ LinkedList/src/InsertionSort.java
@@ -1,3 +1,19 @@
+/*
+class Node
+{
+	int data;
+	Node next;
+	public Node()
+	{
+		this.next=null;
+	}
+	
+	public Node(int data)
+	{
+		this.data=data;
+		this.next=null;
+	}
+}*/
 
 public class InsertionSort 
 {
