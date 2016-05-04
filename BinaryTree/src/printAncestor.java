8d55c9f8c98eb2a74ae2c3653a70908172f805af
Ashish Barthwal <ashishb619@gmail.com>
2016-05-04 16:29:21 +0530

    print all ancestors of a tree

---
+++ BinaryTree/src/printAncestor.java
@@ -0,0 +1,42 @@
+// Print all the ancestor of a binary tree node
+public class printAncestor 
+{
+	public static boolean ancestor(TreeNode root, int n)
+	{
+		if(root==null)
+		{
+			return false;
+		}
+
+		if(root.data==n)
+		{
+			return true;
+		}
+
+		 /* If target is present in either left or right subtree of this node,
+	     then print this node */
+		if( ancestor(root.left,n) || ancestor(root.right,n))
+		{
+			System.out.print(root.data+" ");
+			return true;
+		}
+
+		return false;
+
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		TreeNode root = new TreeNode(1);
+		  root.left        = new TreeNode(2);
+		  root.right       = new TreeNode(3);
+		  root.left.left  = new TreeNode(4);
+		  root.left.right = new TreeNode(5);
+		  root.left.left.left  = new TreeNode(7);
+		 
+		  ancestor(root, 7);
+
+	}
+
+}
