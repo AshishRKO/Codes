24d5b2d79d81e6c8695876fa656150e03defa603
ashishbar404509 <wb-ashishbar404509@antfin.com>
2018-12-24 20:12:04 +0530

    Trie

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Q
+!BinarySearchTree/src/PairSum.java,9/e/9e86e78dacb2731a9f616b9d2b9b18da034b47c6
+[
++BinarySearchTree/src/Graphs/GraphPrint.java,4/0/40bf20a483a3a098016433c3c8d4f34e96f12f45
---
+++ BinarySearchTree/src/TrieInsert.java
@@ -0,0 +1,121 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+
+import java.util.HashMap;
+
+/**
+ * @author Ashish Barthwal
+ * @version $Id: TrieInsert.java, v 0.1 2018-12-24 4:11 PM Ashish Barthwal $$
+ */
+public class TrieInsert {
+
+    private static TrieNode root;
+    public TrieInsert() {
+        root = new TrieNode();
+    }
+
+    private class TrieNode
+    {
+        HashMap<Character, TrieNode> children;
+
+        // isEndOfWord is true if the node represents
+        // end of a word
+        boolean isEndOfWord;
+
+        public TrieNode() {
+            children = new HashMap<>();
+            isEndOfWord = false;
+        }
+
+    }
+
+
+    public boolean search(String word)
+    {
+        TrieNode current = root;
+
+        for(int i=0;i<word.length();i++)
+        {
+            char ch = word.charAt(i);
+
+            TrieNode nextNode = current.children.get(ch);
+
+            if(nextNode == null)
+            {
+                return false;
+            }
+            current = nextNode;
+
+        }
+
+        return current.isEndOfWord;
+    }
+
+    public void insert(String word)
+    {
+        TrieNode current = root;
+
+        for(int i=0;i<word.length();i++)
+        {
+            char ch = word.charAt(i);
+
+            TrieNode nextNode = current.children.get(ch);
+
+            if(nextNode == null)
+            {
+                nextNode = new TrieNode();
+                current.children.put(ch,nextNode);
+            }
+            current = nextNode;
+        }
+
+        current.isEndOfWord = true;
+    }
+
+
+     void display(TrieNode root, char str[], int level)
+    {
+        // If node is leaf node, it indiicates end
+        // of string, so a null charcter is added
+        // and string is displayed
+        if (root.isEndOfWord)
+        {
+            //str[level] = '\0';
+            System.out.println(str);
+            //cout << str << endl;
+        }
+
+        int i;
+        for (i = 0; i < 26; i++)
+        {
+            // if NON NULL child is found
+            // add parent key to str and
+            // call the display function recursively
+            // for child node
+            char ch = (char)(i+65);
+            if (root.children.get(ch) != null)
+            {
+                str[level] = (char)(i + 'A');
+                display(root.children.get(ch), str, level + 1);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        TrieInsert trieInsert = new TrieInsert();
+
+        trieInsert.insert("ASHIISH");
+        trieInsert.insert("ASH");
+        trieInsert.insert("ASHISH");
+
+
+        char str[] = new char[7];
+        trieInsert.display(root, str, 0);
+
+        System.out.println(trieInsert.search("ASH"));
+    }
+}
